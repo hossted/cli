6aaee14e505047c5651097054901c27d2d58e0c6
Lior Kesos <lior@linnovate.net>
2022-02-27 22:05:59 +0200

    Addign config load

--- cmd/register.go
+++ cmd/register.go
@@ -8,27 +8,73 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // registerCmd represents the register command
 var registerCmd = &cobra.Command{
 	Use:   "register",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Register your application with the hossted ecosystem",
+	Long: `The hossted register commands asks for your email and organization name 
+	and registers you with the hossted platfrom
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The hossted platform provides secure and hardened docker images and provides 
+best practices such as tracking updates , monitoring, centralized logging , backups and much more `,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("register called")
+
+		/*
+				if len(args) >= 1 && args[0] != "" {
+					var Arguments = args[0]
+				}
+
+			for _, e := range os.Environ() {
+				pair := strings.SplitN(e, "=", 2)
+				fmt.Println(pair[0])
+			}
+
+				URL := "https://github.com/scraly/gophers/raw/main/" + gopherName + ".png"
+
+				fmt.Println("Try to get '" + gopherName + "' Gopher...")
+
+				// Get the data
+				response, err := http.Get(URL)
+				if err != nil {
+					fmt.Println(err)
+				}
+				defer response.Body.Close()
+
+				if response.StatusCode == 200 {
+					// Create the file
+					out, err := os.Create(gopherName + ".png")
+					if err != nil {
+						fmt.Println(err)
+					}
+					defer out.Close()
+
+					// Writer the body to file
+					_, err = io.Copy(out, response.Body)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					fmt.Println("Perfect! Just saved in " + out.Name() + "!")
+				} else {
+					fmt.Println("Error: " + gopherName + " not exists! :-(")
+				}
+		*/
 	},
+}
+
+func getHosstedDetails() {
+	var hosstedir = viper.GetString("hosstedir") // case-insensitive Setting & Getting
+	fmt.Println("hosstedir:", hosstedir)
 }
 
 func init() {
 	rootCmd.AddCommand(registerCmd)
-
 	// Here you will define your flags and configuration settings.
+	getHosstedDetails()
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
--- cmd/root.go
+++ cmd/root.go
@@ -1,31 +1,36 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 Lior Kesos lior@hossted.com
 
 */
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "hossted",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
+var (
+	cfgFile string
+	rootCmd = &cobra.Command{
+		Use:   "hossted",
+		Short: "A brief description of your application",
+		Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		// Run: func(cmd *cobra.Command, args []string) { },
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -45,7 +50,46 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cobra.yaml)")
+
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
 
+		// Search config in home directory with name ".cobra" (without extension).
+		viper.AddConfigPath(home)
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".hossted")
+	}
+
+	viper.AutomaticEnv()
+	fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+	return
+}
+
+//
+// This script implements the unix cat.
+//
+func cat(r io.Reader) ([]byte, error) {
+	data, err := ioutil.ReadAll(r)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
