82618a8e044d6272b1d0614ea5321d96d2f710e0
Billy Lam <0chun0@gmail.com>
2022-03-28 23:00:22 +0800

    update description of structs

--- hossted/struct.go
+++ hossted/struct.go
@@ -10,6 +10,9 @@ type Config struct {
 	Applications []ConfigApplication `yaml:"applications"`
 }
 
+// ConfigApplication is the applications installled in the vm.
+// Currently it will look up the values from /opt/linnovate/run/software.txt
+// Supporting multiple values for future enhancement.
 type ConfigApplication struct {
 	AppName string `yaml:"appName"`
 	AppPath string `yaml:"appPath"`
@@ -24,7 +27,7 @@ type HosstedRequest struct {
 	SessionToken string            // Session token. JWT
 }
 
-// RegisterResponse
+// RegisterResponse is the return response from the register api
 type RegisterResponse struct {
 	StatusCode int    `json:"status"`
 	Message    string `json:"msg"`
@@ -32,16 +35,22 @@ type RegisterResponse struct {
 	URL        string `json:"url"`
 }
 
+// AvailableCommand is the predefined app/command mapping.
+// Maintained with the command.go file
 type AvailableCommand struct {
 	Apps []App `yaml:"apps"`
 }
 
+// App is a struct to save the available commands for a particular application
+// The length of Commands and Values is expected to be the same, to give additional
+// information or examples to users, on to call the command
 type App struct {
 	App      string   `yaml:"app"`
 	Commands []string `yaml:"commands"`
 	Values   []string `yaml:"values"`
 }
 
+// Command is the individual command, with the app and example values information
 type Command struct {
 	App     string
 	Command string
@@ -51,3 +60,8 @@ type Command struct {
 // AvailableCommandMap saves the map for available commands
 // e.g. map["app.command"] -> [{app command value} {prometheus url example.com} ]
 type AvailableCommandMap map[string]Command
+
+type YAMLSetting struct {
+	Pattern   string   // regex
+	NewValues []string // value of the new input
+}
