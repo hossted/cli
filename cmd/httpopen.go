762b4235686e22345f6e83b5fc49e004dbe9f8bb
Billy Lam <0chun0@gmail.com>
2022-04-10 10:13:56 +0800

    init httpopen

---
+++ cmd/httpopen.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"github.com/hossted/cli/hossted"
+	"github.com/spf13/cobra"
+)
+
+// httpopenCmd represents the httpopen command
+var httpopenCmd = &cobra.Command{
+	Use:     "httpopen",
+	Short:   "[h] httpopen appname",
+	Long:    "[h] httpopen appname",
+	Aliases: []string{"h"},
+	Example: `
+  hossted httpopen <app_name> (e.g. hossted httpopen gitbucket)
+  hossted h gitbucket
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		// Check if the user provides the apps name manually
+		var input string
+		if len(args) >= 1 {
+			input = args[0]
+		}
+		err := hossted.HttpOpen(input)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpopenCmd)
+
+}
