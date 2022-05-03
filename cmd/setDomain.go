ec8fb660e964fa7d7185a8d704b421ca4dd00e5f
Billy Lam <0chun0@gmail.com>
2022-05-03 23:19:18 +0800

    add default app logic

--- cmd/setDomain.go
+++ cmd/setDomain.go
@@ -7,6 +7,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/hossted/cli/hossted"
 	"github.com/spf13/cobra"
@@ -25,14 +26,29 @@ var setDomainCmd = &cobra.Command{
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		if len(args) < 2 {
+		var app, domain string
+
+		if len(args) == 1 {
+			config, err := hossted.GetConfig()
+			if err != nil {
+				return err
+			}
+
+			pwd := hossted.GetCurrentDirectory()
+			app, _ = config.GetDefaultApp(pwd)
+			domain = args[0]
+
+		} else if len(args) == 2 {
+			app = args[0]
+			domain = args[1]
+
+		}
+
+		if strings.TrimSpace(app) == "" {
 			fmt.Printf("Not enough arguments. Expecting <AppName> <Domain>\nPlease checking the input params. %v\n\n", args)
 			cmd.Help()
 			os.Exit(0)
 		}
-
-		app := args[0]
-		domain := args[1]
 
 		err := hossted.SetDomain(app, domain)
 		if err != nil {
