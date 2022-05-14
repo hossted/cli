c6a61dc6d9d1ed9f94a6dc86546b667fe25246a8
Billy Lam <0chun0@gmail.com>
2022-05-14 15:30:03 +0800

    add util for bool

--- cmd/setAuth.go
+++ cmd/setAuth.go
@@ -6,7 +6,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/hossted/cli/hossted"
 	"github.com/spf13/cobra"
 )
 
@@ -17,11 +20,48 @@ var setAuthCmd = &cobra.Command{
 	Long:    `[a] Set authorization of the provided application`,
 	Aliases: []string{"a"},
 	Example: `
+  hossted set auth <AppName> false
   hossted set auth <AppName> true
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		fmt.Println("setAuth called")
+		// Check if the user provides the apps name manually
+		var (
+			app  string
+			flag bool
+		)
+
+		if len(args) < 1 {
+			cmd.Help()
+			os.Exit(0)
+		}
+
+		if len(args) == 1 { // set auth true
+			flag, err := hossted.ConvertBool(args[0])
+			if err != nil {
+				return err
+			}
+
+			config, err := hossted.GetConfig()
+			if err != nil {
+				return err
+			}
+
+			pwd := hossted.GetCurrentDirectory()
+			app, _ = config.GetDefaultApp(pwd)
+
+		} else if len(args) == 2 {
+			app = args[0]
+			flag = args[1]
+		}
+		if strings.TrimSpace(app) == "" {
+			return fmt.Errorf("No input application.")
+		}
+		err := hossted.HttpOpen(app)
+		if err != nil {
+			return err
+		}
+
 		return nil
 	},
 }
