10e31f76da95f2c911bf63f82d6e4740f670074c
Billy Lam <0chun0@gmail.com>
2022-06-28 21:28:02 +0800

    set up for parsing docker compose file

--- hossted/dev.go
+++ hossted/dev.go
@@ -1,13 +1,35 @@
 package hossted
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+type Dummy struct {
+	Head     []string
+	Version  string
+	Services []Service
+	Bottom   []string
+}
+
+type Line struct {
+	LineNum int
+	Line    string
+}
+
+type Service struct {
+	Name    string
+	Content string
+}
 
 func Dev() error {
-	path, err := GetSoftwarePath()
+
+	fmt.Println("Dev")
+	b, err := ioutil.ReadFile("docker-compose.yaml")
 	if err != nil {
 		return err
 	}
-	fmt.Println(path)
+	fmt.Println(string(b))
 
 	return nil
 }
