9caf4bc3715808f60e8478a8b98165fcd381c2a5
AdheipSingh <adheip1222@gmail.com>
2024-03-27 23:54:30 +0530

    updated import subcommand

--- cmd/import.go
+++ cmd/import.go
@@ -1,15 +1,15 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/hossted/cli/hossted"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
-
-
 
 // importCmd represents the import command
 var importCmd = &cobra.Command{
@@ -18,12 +18,34 @@ var importCmd = &cobra.Command{
 	Long:  `hossted import - Import existing app and send info to hossted API`,
     Example: `hossted import k8s`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) == 0 {
-			cmd.Help()
-			panic(1)
+		if t == "k8s" {
+			// err := isValidEmail(args[1])
+			// if err == false {
+			// 	fmt.Println("Invalid email")
+			// }
+			// validate := func(input string) error {
+			// 	fmt.Println("execution")
+			// 	err := strings.Contains(input, "@") && strings.Contains(input, ".")
+			// 	if err != false {
+			// 		fmt.Println("Invalid email")
+			// 		os.Exit(0)
+			// 	}
+			// 	return nil
+			// }
+			prompt := promptui.Prompt{
+				Label: "email",
+			}
+			email_id, _ := prompt.Run()
+			contexts := hossted.GetK8sContext()
+
+			promptk8s := promptui.Select{
+				Label: "Select kubernetes cluster",
+				Items: contexts,
+			}
+			_, cluster, _ := promptk8s.Run()
+			// send request to API
+			fmt.Println("email is", email_id, "cluster selected is", cluster)
 		}
-		importType := args[0]
-		println(importType)
 
 		/*
 		Uuid      string `json:"uuid,omitempty"`
@@ -32,7 +54,6 @@ var importCmd = &cobra.Command{
 	Status    string `json:"status,omitempty"`
 	Test_mode string `json:"test_mode,omitempty"`
 	Comment   string `json:"comment,omitempty"`
-		*/
 		kluster := hossted.KCluster{
 			Uuid:      uuid,
 			Cloud:     cloud,
@@ -42,10 +63,14 @@ var importCmd = &cobra.Command{
 		}
 		authorization = "FrTc3TlygOaFDQOGmteaQ7LRwKOx8XNIGfmLa5NA"
 		hossted.Import(ENVIRONMENT, authorization, kluster)
+
+			*/
+
 	},
 }
 
 var (
+	t string
 	Uuid      string 
 	User      string 
 	Cloud     string 
@@ -56,16 +81,18 @@ var (
 
 func init() {
 	rootCmd.AddCommand(importCmd)
-	importCmd.Flags().StringVarP(&uuid, "uuid", "", "", "")
-	importCmd.Flags().StringVarP(&ip, "ip", "", "", "")
-	importCmd.Flags().StringVarP(&user, "user", "", "", "")
-	importCmd.Flags().StringVarP(&url, "url", "", "", "")
-	importCmd.Flags().StringVarP(&product, "product", "", "", "")
-	importCmd.Flags().StringVarP(&cpunum, "cpunum", "", "", "")
-	importCmd.Flags().StringVarP(&mem, "mem", "", "", "")
-	importCmd.Flags().StringVarP(&rootsize, "rootsize", "", "", "")
-	importCmd.Flags().StringVarP(&cloud, "cloud", "", "", "")
-	importCmd.Flags().StringVarP(&image, "image", "", "", "")
-	importCmd.Flags().StringVarP(&status, "status", "", "", "")
-	importCmd.Flags().StringVarP(&authorization, "authorization", "", "", "")
+	importCmd.Flags().StringVarP(&t, "type", "t", "", "supported env type k8s|docker")
+
+	// importCmd.Flags().StringVarP(&uuid, "uuid", "", "", "")
+	// importCmd.Flags().StringVarP(&ip, "ip", "", "", "")
+	// importCmd.Flags().StringVarP(&user, "user", "", "", "")
+	// importCmd.Flags().StringVarP(&url, "url", "", "", "")
+	// importCmd.Flags().StringVarP(&product, "product", "", "", "")
+	// importCmd.Flags().StringVarP(&cpunum, "cpunum", "", "", "")
+	// importCmd.Flags().StringVarP(&mem, "mem", "", "", "")
+	// importCmd.Flags().StringVarP(&rootsize, "rootsize", "", "", "")
+	// importCmd.Flags().StringVarP(&cloud, "cloud", "", "", "")
+	// importCmd.Flags().StringVarP(&image, "image", "", "", "")
+	// importCmd.Flags().StringVarP(&status, "status", "", "", "")
+	// importCmd.Flags().StringVarP(&authorization, "authorization", "", "", "")
 }
