3f8a081bc089bc7748ed3bed8f78061fda3d7cdf
AdheipSingh <adheip1222@gmail.com>
2024-04-05 03:42:31 +0530

    clean up

--- hossted/service/login.go
+++ hossted/service/login.go
@@ -2,46 +2,38 @@ package service
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/hossted/cli/hossted"
 )
 
-type LoginResponse struct {
-	Success bool                `json:"success"`
-	OrgIDs  []map[string]string `json:"org_ids"`
-	Message string              `json:"message"`
-	Token   string              `json:"token"`
-}
-
 func Login() error {
-	fmt.Println("login")
+
 	email, err := hossted.EmailPrompt()
 	if err != nil {
 		return err
 	}
-	fmt.Println("logging in with email", email)
-	postRequest(email)
+	err = postRequest(email)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func postRequest(email string) error {
 	authToken := os.Getenv("HOSSTED_AUTH_TOKEN")
+
 	payloadStr := fmt.Sprintf(`{"email": "%s"}`, email)
-	url := "https://api.dev.hossted.com/v1/instances/cli/login"
-	payload := []byte(payloadStr)
-	fmt.Println(string(payload))
+
+	url := os.Getenv("HOSSTED_API_URL")
 
 	// Create HTTP request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(payloadStr)))
 	if err != nil {
-		fmt.Println("Error creating request:", err)
 		return err
 	}
 
@@ -49,116 +41,38 @@ func postRequest(email string) error {
 	req.Header.Set("Content-Type", "application/json")
 
 	// Add Authorization header with Basic authentication
-	authHeader := fmt.Sprintf("Basic %s", []byte(authToken))
-	req.Header.Set("Authorization", authHeader)
+	req.Header.Set("Authorization", fmt.Sprintf("Basic %s", []byte(authToken)))
 	// Perform the request
 	client := http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
 		return err
 	}
-	defer resp.Body.Close()
-	// TODO: create structure for response, save response in struct
 
-	// Read and display the re
+	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
-	var response LoginResponse
-	err = json.Unmarshal(body, &response)
-	// err = json.NewDecoder(resp.Body).Decode(&response)
-	if err != nil {
-		return err
-	}
-	// response_byte, err := json.Marshal(response)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	fmt.Println("Response is", response)
-	// fmt.Println("Response:", string(response_byte))
-	// Save response in file
-	// saveResponse(email, response)
+
+	saveResponse(email, body)
 
 	return nil
 }
 
-func saveResponse(email string, result []byte) {
+func saveResponse(email string, data []byte) error {
 	homeDir, err := os.UserHomeDir()
+
 	folderPath := filepath.Join(homeDir, ".hossted")
 	if err != nil {
-		fmt.Println("Error getting home directory:", err)
-		return
-	}
-	// Check and create .hossted directory in user's home
-	setDir(folderPath)
-	// Save the response in file.
-	updateFile(folderPath, email, result)
-
-}
-
-func updateFile(folderPath string, email string, result []byte) {
-	filename := strings.Replace(email, "@", "-", -1)
-	filename += ".json"
-	filePath := filepath.Join(folderPath, filename)
-	// Create the file if it doesn't exist
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		file, err := os.Create(filePath)
-		if err != nil {
-			fmt.Println("Error creating config file:", err)
-			return
-		}
-		file.Write(result)
-		file.Close()
-	} else if os.IsExist(err) {
-		// truncate and update the existing file
-		// ioutil.Write
-		fmt.Println("File exists")
-		file, err := os.OpenFile(filePath, os.O_TRUNC, 0644)
-		if err != nil {
-			fmt.Println("Error opening config file:", err)
-			return
-		}
-		err = file.Close()
-		if err != nil {
-			fmt.Println("Error while saving in config file:", err)
-			return
-		}
-		writeToFile(filePath, result)
-	} else {
-		fmt.Println("Unable to check the config file", err)
-		return
-	}
-}
-func setDir(folderPath string) {
-	// Check if the folder exists in the home directory
-	// Create the folder if it doesn't exist
-	if _, err := os.Stat(folderPath); os.IsNotExist(err) {
-		err := os.Mkdir(folderPath, 0755)
-		if err != nil {
-			fmt.Println("Error creating folder:", err)
-			return
-		}
-		fmt.Println("Folder created at:", folderPath)
+		return err
 	}
 
-}
-
-func writeToFile(filePath string, result []byte) {
-	file, err := os.OpenFile(filePath, os.O_RDWR|os.O_CREATE, 0644)
+	err = ioutil.WriteFile(folderPath+"/"+email+".json", data, 0644)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		return err
 	}
-	_, err = file.Write(result)
-	if err != nil {
-		fmt.Println("Error writing in file:", err)
-		return
-	}
-	err = file.Close()
-	if err != nil {
-		fmt.Println("Error while saving in file:", err)
-		return
-	}
+
+	return nil
 }
